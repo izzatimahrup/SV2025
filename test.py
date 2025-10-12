77dc9219e546aedc809a3356cd6ceaf24189d16f
izzatimahrup <n.izzatimahrup@gmail.com>
2025-10-12 16:14:30 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.setimport pandas as pd
+import plotly.express as px_page_config(
+    page_title="Scientific Visualization"
+)
+
+st.header("Scientific Visualization", divider="gray")
