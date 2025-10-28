a9d6fadb8203f953f912b3fd1f4b4c1ed6d39b3d
izzatimahrup <n.izzatimahrup@gmail.com>
2025-10-29 00:20:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Student Survey:
+    page_title="Student Survey"
 )
 
 visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon="material/school:")
 
-home =  st.Page('home.py', title='HomePage', default-True, icon="material/home;")
+home = st.Page('home.py', title='HomePage', default-True, icon="material/home;")
 
 pg = st.navigation(
     {
@@ -14,4 +14,4 @@ pg = st.navigation(
     }
 )
 
-pg.run
+pg.run()
