ba9178924d77d22bb6d122239fbfd54dea2592ef
izzatimahrup <n.izzatimahrup@gmail.com>
2025-10-29 00:47:57 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,13 @@ st.set_page_config(
     page_title="Student Survey"
 )
 
-visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik', icon='material/school:')
-home = st.Page('home.py', title='Homepage', default=True, icon='material/home:')
+visualise = st.Page('studentSurvey.py', title="Pencapaian Akademik Pelajar', icon=":material/school:")
+
+
 
 pg = st.navigation(
     {
-        "Menu": [home, visualise]
+        "Menu" : [home, visualise]
     }
 )
 
