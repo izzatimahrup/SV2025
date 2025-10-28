ebc9d2d750a40a745ed983d755a3ae5903fa3d6e
izzatimahrup <n.izzatimahrup@gmail.com>
2025-10-29 00:55:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ st.set_page_config(
 
 visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=':material/school:')
 
-home = st.page('home.py', title='HomePage', default=True, icon=':material/school:')
+home = st.Page('home.py', title='HomePage', default=True, icon=':material/school:')
 
 
 
