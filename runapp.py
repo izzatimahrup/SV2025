f6883eb52bce14d35425296f96eeede65585ef1e
izzatimahrup <n.izzatimahrup@gmail.com>
2025-10-29 00:37:38 +0800

    Create runapp.py

---
+++ runapp.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Student Survey"
+)
+
+visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik', icon='material/school:')
+home = st.Page('home.py', title='Homepage', default=True, icon='material/home:')
+
+pg = st.navigation(
+    {
+        "Menu": [home, visualise]
+    }
+)
+
+pg.run()
