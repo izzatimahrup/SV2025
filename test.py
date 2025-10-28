77c4578f42aa8add5c8b93fbb255549cd37d6095
izzatimahrup <n.izzatimahrup@gmail.com>
2025-10-29 00:27:14 +0800

    Fix formatting issues in test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ st.set_page_config(
     page_title="Student Survey"
 )
 
-visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=':material/school:')
 
-home = st.Page('home.py', title='HomePage', default-True, icon=":material/home:")
+home = st.Page('home.py', title='HomePage', default-True, icon=':material/home:')
 
 pg = st.navigation(
     {
