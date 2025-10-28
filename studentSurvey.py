1e7b7d522780264053ace46d5ecf75b2043ea4af
izzatimahrup <n.izzatimahrup@gmail.com>
2025-10-29 01:19:50 +0800

    Update studentSurvey.py

--- studentSurvey.py
+++ studentSurvey.py
@@ -26,10 +26,10 @@ except Exception as e:
 
 col1,col2,col3,col4 = st.columns(4)
 
-col1.metric(label="PLO 2", value-f"3.3", help="PLO 2: Cognitive Skill", border=True)
-col1.metric(label="PLO 3", value-f"3.5", help="PLO 3: Digital Skill", border=True)
-col1.metric(label="PLO 4", value-f"4.0", help="PLO 4: Interpersonal Skill", border=True)
-col1.metric(label="PLO 5", value-f"4.3", help="PLO 5: Communication SKill", border=True)
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col1.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col1.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col1.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication SKill", border=True)
 
 # 1. GPA Calculations
 gpa_cols = [col for col in arts_df.columns if "semester" in col.lower()]
